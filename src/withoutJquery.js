d7a444c02659e5cd4057967eac95c7865c67f7e3
tumiki-4092 <trbm.tumiki.4092@gmail.com>
2022-02-21 13:40:35 +0900

    test

--- src/withoutJquery.js
+++ src/withoutJquery.js
@@ -1,5 +1,48 @@
 'use strict';
-function module(){
+
+Object.defineProperty(HTMLElement.prototype,"wJFadeIn",{
+    configurable:true,
+    enumerable:false,
+    writable:true,
+    value:function(d,tf){
+        //default arg
+        if(!d)d=300;
+        if(!tf)tf="linear";
+        if(window.getComputedStyle(this).display!=="none") return;
+        if(this.style.display==="none"){
+            this.style.display="";
+        }
+        else{
+            this.style.display="block";
+            this.style.opacity=0;
+        }
+        let start=performance.now();
+        requestAnimationFrame(function animation(timestamp){
+            let easing=undefined;
+            let progress=(timestamp-start);
+            switch(tf){
+                case "linear":
+                    easing=progress/d;
+                    break;
+                case "easeIn":
+                    easing=1-Math.cos((progress*Math.PI)/2);
+                    break;
+                case "easeOut":
+                    easing=Math.sin((progress*Math.PI)/2);
+                    break;
+                case "easeInOut":
+                    easing=((Math.cos(Math.PI*progress)-1)/2)*-1;
+                    break;
+                default:
+                    break;
+            };
+            this.style.opacity=Math.min(easing,1);
+            if(easing<1)requestAnimationFrame(animation);
+            else this.style.opacity="";
+        });
+    }
+});
+function withoutJquery(){
     this.accordion=function(){
         this.close=function(e,d,tf){
             //default arg
